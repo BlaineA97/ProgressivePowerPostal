ba82cfe118e10c0a316dc5b207460be6db8b61f2
Blaine Anderson <BlaineA97@Gmail.com>
2018-02-20 11:29:43 -0700

    css is responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './Header.css';
 import Navigation from './Navigation';
+import backgroundOne from '../images/storefront.png'
 
 class Header extends Component {
   render() {
     return (
       <div className="Header-container">
+        {/* <img id="Header-background" src={backgroundOne} alt="StoreImage"/> */}
         <Navigation />
       </div>
     );
