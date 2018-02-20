a2ec3b9601803c0e3b45ffcd56c54d512bb8fa0b
Blaine Anderson <BlaineA97@Gmail.com>
2018-02-20 11:55:23 -0700

    added image background selectors based onClick

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import './Header.css';
 import Navigation from './Navigation';
-import backgroundOne from '../images/storefront.png'
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+    this.state = {
+      backgroundSelection: "Header-background-1"
+    };
+  }
+
+  onClick() {
+    if (this.state.backgroundSelection === "Header-background-1") {
+      this.setState({
+        backgroundSelection: "Header-background-2"
+      })
+    } else if (this.state.backgroundSelection === "Header-background-2") {
+      this.setState({
+        backgroundSelection: "Header-background-3"
+      })
+    } else {
+      this.setState({
+        backgroundSelection: "Header-background-1"
+      })
+    }
+  }
+
   render() {
     return (
-      <div className="Header-container">
-        {/* <img id="Header-background" src={backgroundOne} alt="StoreImage"/> */}
+      <div id={this.state.backgroundSelection} className="Header-container">
+
         <Navigation />
+        {this.state.backgroundSelection}
+        <div className="Header-arrow-container">
+          <a onClick={this.onClick}> press me </a>
+        </div>
+
       </div>
     );
   }
