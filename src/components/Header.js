60939215d4e4686182ac443264179b7b49cc0fca
Blaine Anderson <BlaineA97@Gmail.com>
2018-02-20 12:26:08 -0700

    added text for each image background that changes automatically

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,29 +6,46 @@ class Header extends Component {
   constructor(props) {
     super(props);
     this.onClick = this.onClick.bind(this);
+    this.imageTextOne = this.imageTextOne.bind(this);
     this.state = {
-      backgroundSelection: "Header-background-1"
+      backgroundSelection: "Header-background-1",
+      backgroundText: this.imageTextOne()
     };
   }
 
+  imageTextOne() {
+    return(<p>"Affordable & Reliable Shipping Services"</p>)
+  }
+  imageTextTwo() {
+    return(<p>"Candles, Greeting Cards & Office Supplies"</p>)
+  }
+  imageTextThree() {
+    return(<p>"Packing Services & Supplies"</p>)
+  }
+
+// Alternates Header background images when clicked
   onClick() {
     if (this.state.backgroundSelection === "Header-background-1") {
       this.setState({
-        backgroundSelection: "Header-background-2"
+        backgroundSelection: "Header-background-2",
+        backgroundText: this.imageTextTwo()
       })
     } else if (this.state.backgroundSelection === "Header-background-2") {
       this.setState({
-        backgroundSelection: "Header-background-3"
+        backgroundSelection: "Header-background-3",
+        backgroundText: this.imageTextThree()
       })
     } else {
       this.setState({
-        backgroundSelection: "Header-background-1"
+        backgroundSelection: "Header-background-1",
+        backgroundText: this.imageTextOne()
       })
     }
   }
 
+// Clicks "onClick" every 5 seconds to change the background picture automatically
   componentDidMount() {
-    this.state.backgroundSelection = setInterval(this.onClick, 5000);
+    this.state.backgroundSelection = setInterval(this.onClick, 3000);
   }
 
   render() {
@@ -37,7 +54,7 @@ class Header extends Component {
 
         <Navigation />
         <div className="Header-arrow-container">
-          <a onClick={this.onClick}> press me </a>
+          <a onClick={this.onClick}> {this.state.backgroundText} </a>
         </div>
 
       </div>
