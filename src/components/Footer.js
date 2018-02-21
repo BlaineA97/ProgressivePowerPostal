04d47f34884dc9a133a5944f46cfb6566c148ee5
Blaine Anderson <BlaineA97@Gmail.com>
2018-02-21 00:23:08 -0700

    Created Footer. Added links to ReactJS and Blaine Anderson github, added comeplete CSS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 import './Footer.css';
 
 class Footer extends Component {
+
+  openLinkReactJS() {
+    window.open("https://reactjs.org/", '_blank');
+  }
+
+  openLinkBlaine() {
+    window.open("https://github.com/BlaineA97", '_blank');
+  }
+
   render() {
     return (
-      <div className="Footer">
-        <h1>This is: Footer</h1>
+      <div className="Footer-container">
+        <div id="Footer-center">
+          <p className="Footer-text">© 2015 Power Postal at Las Sendas</p>
+          <p className="Footer-text-link" onClick={() => this.openLinkBlaine()}>Built by Blaine Anderson</p>
+          <p className="Footer-text-link" onClick={() => this.openLinkReactJS()}>Powered with ReactJS</p>
+        </div>
       </div>
     );
   }
