6ee56673febc72315122dc6f3d627801a046b608
Blaine Anderson <BlaineA97@Gmail.com>
2018-02-23 12:34:17 -0700

    added dropdown menu with correct alignment

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,49 +7,103 @@ import MediaQuery from 'react-responsive';
 class Navigation extends Component {
 
   render() {
+    /* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
     return (
-      <div className="Navigation-container">
-
-        <div className="Navigation-flexbox-left">
-          <Link to="/" className="Navigation-logo">
-            <h1 className="Navigation-logo-1">Power Postal</h1>
-            <h3 className="Navigation-logo-2">at Las Sendas</h3>
-          </Link>
-        </div>
-
-        <div className="Navigation-flexbox-center">
-        </div>
+      <div>
 
         <MediaQuery maxWidth={499}>
-          <div className="Navigation-flexbox-right-dropdown">
-            <button onclick="myFunction()" class="Navigation-drop-menu">
-              <div className="Navigation-drop-button"></div>
-              <div className="Navigation-drop-button"></div>
-              <div className="Navigation-drop-button"></div>
-            </button>
+          <div className="Nav-dropdown">
+            <div className="Nav-dropdown-container">
+
+            <div className="Nav-dropdown-logo">
+              <Link to="/" className="Nav-dropdown-logo-link">
+                <h1 className="Nav-dropdown-logo-1">Power Postal</h1>
+                <h3 className="Nav-dropdown-logo-2">at Las Sendas</h3>
+              </Link>
+            </div>
+
+            <div class="Nav-dropdown-menu-container">
+              <div class="Nav-dropdown-display">
+                <button class="Nav-dropdown-button">
+                  <p id="bar-1" className="Nav-menu-bar"></p>
+                  <p id="bar-2" className="Nav-menu-bar"></p>
+                  <p id="bar-3" className="Nav-menu-bar"></p>
+                </button>
+                <div class="Nav-dropdown-content">
+                  <Link to="/services"><p>Our Services</p></Link>
+                  <Link to="/tracking"><p>Track Package</p></Link>
+                  <Link to="/contact"><p>Contact Us</p></Link>
+                </div>
+              </div>
+            </div>
+
+            {/* <div className="Nav-dropdown-container">
+              <button onclick="myFunction()" class="Nav-drop-button">
+                <div className="Nav-drop-menu">
+                  <p id="bar-1">1</p>
+                  <p id="bar-2">2</p>
+                  <p id="bar-3">3</p>
+                </div>
+              </button>
+            </div> */}
+
           </div>
+            <div className="Nav-dropdown-backdrop"></div>
+          </div>
+
          </MediaQuery>
 
          <MediaQuery minWidth={500} maxWidth={1200}>
-           <div className="Navigation-flexbox-right-full">
-             <div id="Navigation-services">
-               <Link className="Navigation-button" to="/services"><p>Our Services</p></Link>
+           <div className="Nav-container">
+
+             <div className="Nav-flexbox-left">
+               <Link to="/" className="Nav-logo">
+                 <h1 className="Nav-logo-1">Power Postal</h1>
+                 <h3 className="Nav-logo-2">at Las Sendas</h3>
+               </Link>
              </div>
 
-             <div id="Navigation-tracking">
-               <Link className="Navigation-button" to="/tracking"><p>Track Package</p></Link>
+             <div className="Nav-flexbox-center">
              </div>
 
-             <div id="Navigation-contact">
-               <Link className="Navigation-button" to="/contact"><p>Contact Us</p></Link>
-             </div>
-             </div>
+             <div className="Nav-flexbox-right-full">
+               <div id="Nav-services">
+                 <Link className="Nav-button" to="/services"><p>Our Services</p></Link>
+               </div>
 
-             <div className="Navigation-backdrop">
+               <div id="Nav-tracking">
+                 <Link className="Nav-button" to="/tracking"><p>Track Package</p></Link>
+               </div>
+
+               <div id="Nav-contact">
+                 <Link className="Nav-button" to="/contact"><p>Contact Us</p></Link>
+               </div>
+               </div>
+
+               <div className="Nav-backdrop">
+             </div>
            </div>
          </MediaQuery>
-
-      </div>
+        </div>
     );
   }
 }
