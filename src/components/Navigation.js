fbe7fe9a62767be05cc7808faef01b655f9898a7
Blaine Anderson <BlaineA97@Gmail.com>
2018-02-23 18:13:50 -0700

    updated navigation css to include mid range media option, adjusted dropdown menu position

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,7 +10,7 @@ class Navigation extends Component {
     return (
       <div className="Nav-dropdown">
 
-        <MediaQuery maxWidth={799}>
+        <MediaQuery maxWidth={549}>
             <div className="Nav-dropdown-container">
             <div className="Nav-dropdown-logo">
               <Link to="/" className="Nav-dropdown-logo-link">
@@ -34,6 +34,36 @@ class Navigation extends Component {
             </div>
           </div>
           <div className="Nav-dropdown-backdrop"></div>
+         </MediaQuery>
+
+         <MediaQuery minWidth={550} maxWidth={799}>
+           <div className="Nav-full-container">
+             <div className="Nav-full-logo-container">
+               <Link to="/" className="Nav-full-logo-link">
+                 <h1 className="Nav-full-logo-1">Power Postal</h1>
+                 <h3 className="Nav-full-logo-2">at Las Sendas</h3>
+               </Link>
+             </div>
+              <div className="Nav-med-menu">
+                 <div id="Nav-full-services" className="Nav-full-link-div">
+                    <Link to="/services" className="Nav-med-link">
+                      <p className="Nav-full-button">Our Services</p>
+                    </Link>
+                 </div>
+                 <div id="Nav-full-tracking" className="Nav-full-link-div">
+                   <Link to="/tracking" className="Nav-med-link">
+                     <p className="Nav-full-button">Track Package</p>
+                   </Link>
+                 </div>
+                 <div id="Nav-full-contact" className="Nav-full-link-div">
+                   <Link to="/contact" className="Nav-med-link">
+                     <p className="Nav-full-button">Contact Us</p>
+                   </Link>
+                 </div>
+               </div>
+             <div className="Nav-full-backdrop">
+             </div>
+           </div>
          </MediaQuery>
 
          <MediaQuery minWidth={800} maxWidth={1200}>
@@ -65,6 +95,7 @@ class Navigation extends Component {
              </div>
            </div>
          </MediaQuery>
+
         </div>
     );
   }
